ebb5e847a269aba9e39f48bcf238b5944a6ff531
Rubin Zach <zrubin@lcs-mba3136.lcs.on.ca>
2017-10-03 13:58:18 -0400

    i did the thing :D

--- IterationExamples/Iteration Examples.playground/Pages/Exercise.xcplaygroundpage/Contents.swift
+++ IterationExamples/Iteration Examples.playground/Pages/Exercise.xcplaygroundpage/Contents.swift
@@ -30,6 +30,10 @@ let canvas = Canvas(width: 300, height: 300)
 
 // Below this line, try combining a loop and a single statement to generate the goal
 
+for i in stride(from: 0, through: 300, by: 20) {
+    canvas.drawLine(fromX: i, fromY: 0, toX: 300, toY: i)
+}
+
 /*:
  ## Template code
  The code below is necessary to see results in the Assistant Editor at right. Please do not remove.
