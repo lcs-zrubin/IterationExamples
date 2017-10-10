8ecea47512d26c9f08585cfb5f54c33c1217066a
Rubin Zach <zrubin@lcs-mba3136.lcs.on.ca>
2017-10-10 09:21:46 -0400

    Challenge Done

--- IterationExamples/Iteration Examples.playground/Pages/Challenge.xcplaygroundpage/Contents.swift
+++ IterationExamples/Iteration Examples.playground/Pages/Challenge.xcplaygroundpage/Contents.swift
@@ -19,8 +19,8 @@ let canvas = Canvas(width: 300, height: 300)
 for i in stride(from: 0, through: 300, by: 20) {
     canvas.drawLine(fromX: i, fromY: 0, toX: 300, toY: i)
     canvas.drawLine(fromX: i, fromY: 300, toX: 0, toY: i)
-    canvas.drawLine(fromX: 0, fromY: i, toX: i, toY: 300)
-    canvas.drawLine(fromX: 300, fromY: i, toX: i, toY: 0)
+    canvas.drawLine(fromX: 300-i, fromY: 300, toX: 300, toY: i)
+    canvas.drawLine(fromX: 300-i, fromY: 0, toX: 0, toY: i)
 }
 /*:
  ## Template code
