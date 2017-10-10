3620d462a2958f5d4f8dbc44179497277399477d
Rubin Zach <zrubin@lcs-mba3136.lcs.on.ca>
2017-10-10 09:45:50 -0400

    Colours :D

--- IterationExamples/Iteration Examples.playground/Pages/Challenge.xcplaygroundpage/Contents.swift
+++ IterationExamples/Iteration Examples.playground/Pages/Challenge.xcplaygroundpage/Contents.swift
@@ -17,8 +17,9 @@ let canvas = Canvas(width: 300, height: 300)
 // Below this line, try combining a loop and four statements that draw lines to generate the goal
 
 canvas.translate(byX: 150, byY: 150)
-for _ in stride(from: 0, through: 15, by: 1) {
+for i in stride(from: 0, through: 360, by: 30) {
     canvas.rotate(by: 15)
+    canvas.lineColor = Color(hue: i, saturation: 50, brightness: 75, alpha: 100)
     canvas.drawLine(fromX: -150, fromY: 0, toX: 150, toY: 0)
 }
 /*:
